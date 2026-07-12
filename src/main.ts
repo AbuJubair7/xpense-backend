6fd5675105e60d5faf9bf397c7b93e072ef30eca
AbuJubair7 <jubaircode7@gmail.com>
2026-07-12 14:47:54 +0600

    fix(backend): change fallback port to 5001 to match docker EXPOSE

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,7 @@ async function bootstrap() {
     }),
   );
 
-  const port = process.env.PORT ?? 5000;
+  const port = process.env.PORT ?? 5001;
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
   console.log(`Swagger Docs available at: http://localhost:${port}/api`);
